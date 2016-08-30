cfb6d8bf24c46a1e01d9dddfda104ac0fb7a2d0d
Edderic Ugaddan <edderic@gmail.com>
2016-08-30 14:16:26 -0400

    bugsnag scraper to aggregate last seven days events

---
+++ scrape_bugsnag.rb
@@ -0,0 +1,24 @@
+require 'capybara'
+require 'capybara/dsl'
+
+require 'capybara/poltergeist'
+
+include Capybara::DSL
+Capybara.default_driver = :poltergeist
+
+visit "https://app.bugsnag.com/user/sign_in"
+
+fill_in 'Email', with: ENV['BUGSNAG_EMAIL']
+fill_in 'Password', with: ENV['BUGSNAG_PASSWORD']
+
+click_on 'Sign in'
+first('span', text: '7d').click
+
+sleep 5 # required
+
+seven_day_error_occurrences = all(".DataTable-row .DataTable-cell:nth-child(3)").reduce(0) do |accum, node|
+  accum + node.text.to_i
+end
+
+puts "Total of occurrences for 7 days: #{seven_day_error_occurrences}"
+
